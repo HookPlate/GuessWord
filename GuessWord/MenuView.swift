38325566f19cb93b00110a74be00469201e04c29
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-12-12 14:00:13 +0000

    Adds the screen for the starting/ending/playing states

---
+++ GuessWord/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  GuessWord
+//
+//  Created by robin tetley on 12/12/2023.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuView()
+}
